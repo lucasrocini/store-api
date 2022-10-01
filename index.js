7c0e6f9831188b3739f159293dc4aad29a7f82a7
Lucas <lucasrocini@hotmail.com>
2022-10-01 11:10:40 -0300

    primary routers

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import clientsRouter from "./routes/client.route.js";
+import productsRouter from "./routes/product.route.js";
+import suppliersRouter from "./routes/supplier.route.js";
+import salesRouter from "./routes/sale.route.js";
 
 const app = express();
+
+app.use("/client", clientsRouter);
+app.use("/product", productsRouter);
+app.use("/supplier", suppliersRouter);
+app.use("/sale", salesRouter);
 
 app.listen(3000, () => console.log("API Started!"));
