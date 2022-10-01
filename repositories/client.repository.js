b069e898af010059fcbca5308705f545d5ff66b0
Lucas <lucasrocini@hotmail.com>
2022-10-01 14:13:40 -0300

    Creation Connection Pool

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,3 +1,5 @@
+import pg from "pg";
+
 async function insertClient(client){
 
 }
---
+++ repositories/db.js
@@ -0,0 +1,16 @@
+import  pg from "pg";
+
+async function connect() {
+    if (global.connection) {
+        return global.connection.connect();
+    }
+    const pool = new pg.Pool({
+        connectionString: ""
+    });
+    global.connection = pool;
+    return pool.connect();
+}
+
+export {
+    connect
+}
