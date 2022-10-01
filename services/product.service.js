b4612d3e1bf9921c70ba2a3fe27b7aa5ed3b87db
Lucas <lucasrocini@hotmail.com>
2022-10-01 17:02:31 -0300

    updates to validate FK for product creation/update

--- services/product.service.js
+++ services/product.service.js
@@ -1,7 +1,11 @@
 import ProductRepository from "../repositories/product.repository.js"
+import SupplierRepository from "../repositories/supplier.repository.js"
 
 async function createProduct(product){
-    return await ProductRepository.insertProduct(product);
+    if(await SupplierRepository.getSupplier(product.supplier_id)) {
+        return await ProductRepository.insertProduct(product);
+    }
+    throw new Error("Supplier ID informado não existe!")    
 }
 
 async function getProducts(){
@@ -13,7 +17,10 @@ async function getProduct(id){
 }
 
 async function updateProduct(product){
-    return await ProductRepository.updateProduct(product);
+    if(await SupplierRepository.getSupplier(product.supplier_id)) {
+        return await ProductRepository.updateProduct(product);
+    }
+    throw new Error("Supplier ID informado não existe!")  
 }
 
 async function deleteProduct(id){
